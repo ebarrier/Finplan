f30cf0386b3a0f8e986dd15fcf4f8c6122904dff
ebarrier <etienne.barrier@gmail.com>
2016-05-12 15:28:23 +0300

    Login with username or email possible

--- login.php
+++ login.php
@@ -2,46 +2,31 @@
 
 include "header.php";
 require_once "config.php";
-echo "This is the content of the POST request: ";
-var_dump($_POST); ?>
+var_dump($_POST);
 
-<br>
-
-<?php
 $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error)
   die("Connection to database failed:".$conn->connect_error);
 $conn->query("set names utf8");
 
-//function checkCredentials(string $dbFieldToCheck) {
-//    $statement = $conn->prepare("SELECT id FROM user WHERE `".$dbFieldToCheck."` = ? AND password = PASSWORD(?)"); // not to use this in production!
-//    if (!$statement) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-//    $statement->bind_param("ss", $_POST["username/email"], $_POST["password"]);
-//    $statement->execute();
-//    $result = $statement->get_result();
-//    $row = $result->fetch_assoc(); //fetch_assoc stores info as key-value pair
-//    return $row;
-//}
-//$row1 = checkCredentials('email');
-//$row2 = checkCredentials('username');
+function checkCredentials($dbFieldToCheck, $conn, $username, $password) {
+    $statement = $conn->prepare("SELECT id FROM user WHERE ".$dbFieldToCheck." = ? AND password = PASSWORD(?)"); // not to use this in production!
+    if (!$statement) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+    $statement->bind_param("ss", $username, $password);
+    $statement->execute();
+    $result = $statement->get_result();
+    $row = $result->fetch_assoc();
+    return $row;
+}
 
-$statement = $conn->prepare("SELECT id FROM user WHERE email = ? AND password = PASSWORD(?)"); // not to use this in production!
-if (!$statement) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-$statement->bind_param("ss", $_POST["username/email"], $_POST["password"]);
-$statement->execute();
-$result = $statement->get_result();
-$row = $result->fetch_assoc(); //fetch_assoc stores info as key-value pair
+$row1 = checkCredentials('email', $conn, $_POST["username/email"], $_POST["password"]);
+$row2 = checkCredentials('username', $conn, $_POST["username/email"], $_POST["password"]);
 
-$statement2 = $conn->prepare("SELECT id FROM user WHERE username = ? AND password = PASSWORD(?)"); // not to use this in production!
-if (!$statement2) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-$statement2->bind_param("ss", $_POST["username/email"], $_POST["password"]);
-$statement2->execute();
-$result2 = $statement2->get_result();
-$row2 = $result2->fetch_assoc(); //fetch_assoc stores info as key-value pair
+var_dump($row1);
+var_dump($row2);
 
-
-if($row) { //if the key-value pair user_id-password exists
-    $_SESSION["userid"] = $row["id"]; // This just stores user row number
+if($row1) { //if the key-value pair user_id-password exists
+    $_SESSION["userid"] = $row1["id"]; // This just stores user row number
     header('Location: index.php'); //This will redirect back to index.php
 } elseif($row2) {
     $_SESSION["userid"] = $row2["id"];
@@ -50,5 +35,6 @@ if($row) { //if the key-value pair user_id-password exists
   <p>It looks like you are not known sorry. Please <a href="registration.php">sign up</a>  to enjoy our services or go back to <a href="index.php">main page</a>.</p>
 
 <?php 
+var_dump($_SESSION["userid"]);
 } 
 ?>
