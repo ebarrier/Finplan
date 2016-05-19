f956b63c8ff3541080bf6430a8a4fc0262d6b826
ebarrier <etienne.barrier@gmail.com>
2016-05-19 13:18:23 +0300

    Put connection to db in separate dbconn.php file

--- login.php
+++ login.php
@@ -3,18 +3,7 @@
 include "header.php";
 require_once "config.php";
 
-try {
-    $conn = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+include "dbconn.php";
 
 //function to check if the email or username provided match the password
 function checkCredentials($dbFieldToCheck, $conn, $username, $password) {
