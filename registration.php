cd2f32af16d84f3cd2577400052cabecaa14e54a
ebarrier <etienne.barrier@gmail.com>
2016-06-04 02:54:12 +0300

    Add server and client validation to registration form

--- registration.php
+++ registration.php
@@ -7,7 +7,11 @@ include "header.php";
 <form method="post" action="regsubmit.php">
   <div>
     <label for="username">Username</label>
-    <input type="text" name="username" id="username" required/>
+    <input type="text" 
+        name="username" 
+        id="username" 
+        pattern="[\w\.]{1,64}"
+        title ="Numbers, letters (case sensitive), underscore and dot are allowed. 64 characters max" required/>
   </div> 
 
   <div>
@@ -17,22 +21,27 @@ include "header.php";
 
   <div>
     <label for="password1">Password</label>
-    <input type="password" name="password1" id="password1" required/>
+    <input type="password" name="password1" id="password1" pattern=".{8,256}" required/>
   </div>
 
   <div>
     <label for="password2">Repeat password</label>
-    <input type="password" name="password2" id="password2" required/>
+    <input type="password" 
+        name="password2" 
+        id="password2" 
+        onkeyup="checkPass(); return false;"
+        pattern=".{8,256}" required/>
+        <span id="confirmMessage" class="confirmMessage"></span>
   </div>
 
   <div>
     <label for="firstname">First-name</label>
-    <input type="text" name="firstname" id="firstname" required/>
+    <input type="text" name="firstname" id="firstname" pattern="[-a-zA-z]{1,30}" required/>
   </div> 
 
   <div>
     <label for="lastname">Last-name</label>
-    <input type="text" name="lastname" id="lastname" required/>
+    <input type="text" name="lastname" id="lastname" pattern="[-a-zA-z]{1,30}" required/>
   </div> 
 
   <div>
@@ -40,6 +49,11 @@ include "header.php";
   </div>
   
 </form>
+
+<br>
+<a href="index.php">Go back to main page</a>
+
+<script src="js/registration.js"></script>
 
 <?php
 include "footer.php";
--- regsubmit.php
+++ regsubmit.php
@@ -7,38 +7,70 @@ if($_SERVER['REQUEST_METHOD'] != "POST") {
     header("index.php");
 }
 
-//Statement to create user in DB
-$statement = $conn->prepare(
-"INSERT INTO `user` (
-    `username`,
-    `email`,
-    `password`,
-    `fname`,
-    `lname`)
-VALUES (:username, :email, :hashed_password, :firstname, :lastname)"); //the :arguments will be replaced below
+$validFields = true;
 
-if (!$statement) die("Prepare failed: (" . $conn->errno . ") " . $conn->error); //check if an error happens
+if($_POST["username"] == null || preg_match("[\w.]{1,64}", $_POST["username"]) != 1) {
+    $validFields = false;
+    echo "<p>Your username is missing or invalid</p>";
+}
 
-//We bind all the parameters
-$statement->bindParam(':username', $_POST["username"]);
-$statement->bindParam(':email', $_POST["email"]);
-$statement->bindParam(':hashed_password', password_hash($_POST["password"], PASSWORD_DEFAULT));
-$statement->bindParam(':firstname', $_POST["firstname"]);
-$statement->bindParam(':lastname', $_POST["lastname"]);
+if($_POST["email"] == null || preg_match("^[a-z0-9._%+-]+@(?:[a-z0-9-]+\.)+[a-z]{2,}$", $_POST["email"]) != 1) {
+    $validFields = false;
+    echo "<p>Your email is missing or invalid</p>";
+}
 
-//We execute the statement to create user with POST values
-if ($statement->execute()) {
-  echo "Registration successful. Thank you! <br> <a href=\"index.php\">Go back to main page</a>";
-} else {
-    if (!$statement->errno == 1062) {
-      //This is result in 200 OK
-      echo "This e-mail is already registered";
+if($_POST["password1"] == null || preg_match(".{8,256}", $_POST["password1"]) != 1 || $_POST["password1"] !== $_POST["password2"]) {
+    $validFields = false;
+    echo "<p>Your password is missing or invalid</p>";
+}
+
+if($_POST["firstname"] == null || preg_match("[-a-zA-z]{1,30}", $_POST["firstname"]) != 1) {
+    $validFields = false;
+    echo "<p>Your firstname is missing or invalid</p>";
+}
+
+if($_POST["lastname"] == null || preg_match("[-a-zA-z]{1,30}", $_POST["lastname"]) != 1) {
+    $validFields = false;
+    echo "<p>Your lastname is missing or invalid</p>";
+}
+
+if($validFields) {
+    //Statement to create user in DB
+    $statement = $conn->prepare(
+    "INSERT INTO `user` (
+        `username`,
+        `email`,
+        `password`,
+        `fname`,
+        `lname`)
+    VALUES (:username, :email, :hashed_password, :firstname, :lastname)"); //the :arguments will be replaced below
+
+    if (!$statement) die("Prepare failed: (" . $conn->errno . ") " . $conn->error); //check if an error happens
+
+    //We bind all the parameters
+    $statement->bindParam(':username', $_POST["username"]);
+    $statement->bindParam(':email', $_POST["email"]);
+    $statement->bindParam(':hashed_password', password_hash($_POST["password"], PASSWORD_DEFAULT));
+    $statement->bindParam(':firstname', $_POST["firstname"]);
+    $statement->bindParam(':lastname', $_POST["lastname"]);
+
+    //We execute the statement to create user with POST values
+    if ($statement->execute()) {
+      echo "Registration successful. Thank you! <br> <a href=\"index.php\">Go back to main page</a>";
     } else {
-      //This will result in 500 internal server error
-        die("Execute failed: (" . $statement->errno . ") " . $statement->error); //check if an error happens
-      }
-  }
+        if ($statement->errorCode() == 1062) {
+          //This is result in 200 OK
+          echo "This e-mail or username is already registered";
+        } else {
+          //This will result in 500 internal server error
+            die("Execute failed: (" . $statement->errorCode() . ") " . $statement->errorInfo()); //check if an error happens
+          }
+    }
+}
 ?>
+
+<br>
+<a href="registration.php">Go back to registration page</a>
 
 <?php
 include "footer.php";
